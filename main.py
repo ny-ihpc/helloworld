576ba11b3f5f1f324cac5a4f6b1ae5ba7d4bf043
Nay Yaung <6479987+nayyaung@users.noreply.github.com>
2022-05-24 14:58:44 +0800

    added health check endpoint

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
+import json
+
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    print('V2: get call received.')
-    return 'Hello World!!!!!'
+    app.logger.info('V2: get call received.')
+    return json.dumps({'message': 'Hello World'}), 200, {'ContentType': 'application/json'}
+
+
+@app.route('/health')
+def health():
+    return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
+
 
 app.run(host='0.0.0.0', port=8080)
